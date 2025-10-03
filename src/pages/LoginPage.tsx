801a45d63d24d95d14b03a00dfab881282ef48fe
DimasDarma11 <dimasdarma35@gmail.com>
2025-10-04 04:52:02 +0700

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ const LoginPage = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-purple-600">
       <h1 className="text-4xl font-bold text-white animate-bounce">
-        🚧 Coming Soon: Login Page 🚧
+        🚧 Coming Soon: Login Page 🚧 
       </h1>
     </div>
   );

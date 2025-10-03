53ef56ddc76ec6685d60fa19888e78c6088409b8
DimasDarma11 <dimasdarma35@gmail.com>
2025-10-04 04:41:33 +0700

    Create LoginPage.tsx

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-purple-600">
+      <h1 className="text-4xl font-bold text-white animate-bounce">
+        🚧 Coming Soon: Login Page 🚧
+      </h1>
+    </div>
+  );
+};
+
+export default LoginPage;

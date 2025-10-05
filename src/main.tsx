525ce0f729a6da1737a36558e55091b731a40d03
DimasDarma11 <dimasdarma35@gmail.com>
2025-10-05 11:18:07 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import LoginPage from "./pages/LoginPage.tsx"; 
+import Rules from "./pages/Rules.tsx"; 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(

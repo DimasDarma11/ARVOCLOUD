5d015b6a4d29d760c294582df2265bb771676049
Dimas Darma <dimasdarma35@gmail.com>
2025-10-14 23:47:51 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
-import LoginPage from "./pages/LoginPage.tsx"; 
-import Rules from "./pages/Rules.tsx"; 
-import Status from "./pages/Status.tsx"; 
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+import App from './App.tsx'; // ini router utama yg berisi LandingApp & DashboardApp
+import LoginPage from './pages/LoginPage.tsx';
+import Rules from './pages/Rules.tsx';
+import Status from './pages/Status.tsx';
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* App utama yang mengatur landing & dashboard */}
+        <Route path="/*" element={<App />} />
+
+        {/* Halaman tambahan khusus */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/rules" element={<Rules />} />
         <Route path="/status" element={<Status />} />

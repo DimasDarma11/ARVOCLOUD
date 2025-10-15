b50ebef4526997b74948a9375cd1750c2669fda4
Dimas Darma <dimasdarma35@gmail.com>
2025-10-15 19:03:05 +0700

    Update DashboardApp.tsx

--- src/pages/DashboardApp.tsx
+++ src/pages/DashboardApp.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from '../contexts/AuthContext';
 import { ProtectedRoute } from '../components/ProtectedRoute';
 import { Login } from './Login';

bb2e3f9971fea4fe0a46698e729ecb634a3ae4a4
Dimas Darma <dimasdarma35@gmail.com>
2025-10-16 09:39:56 +0700

    Create switch.tsx

---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+interface SwitchProps {
+  checked: boolean;
+  onCheckedChange: (value: boolean) => void;
+}
+
+export function Switch({ checked, onCheckedChange }: SwitchProps) {
+  return (
+    <button
+      onClick={() => onCheckedChange(!checked)}
+      className={`relative inline-flex h-6 w-11 items-center rounded-full transition ${
+        checked ? 'bg-blue-600' : 'bg-gray-300'
+      }`}
+    >
+      <span
+        className={`inline-block h-4 w-4 transform rounded-full bg-white transition ${
+          checked ? 'translate-x-6' : 'translate-x-1'
+        }`}
+      />
+    </button>
+  );
+}

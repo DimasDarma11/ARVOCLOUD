1dbac3fdb9a480764122e1c793342f3a7af8f2df
Dimas Darma <dimasdarma35@gmail.com>
2025-10-21 23:29:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       webp: { quality: 80 },
       avif: { quality: 80 },
     }),
+  ],
 
   optimizeDeps: {
     include: ['lucide-react', 'framer-motion', 'react', 'react-dom'],

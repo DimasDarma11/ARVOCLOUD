aca7362db39ac80c5dc058a339a2939f50ea8076
Dimas Darma <dimasdarma35@gmail.com>
2025-11-23 09:23:02 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LandingApp from './pages/LandingApp';
-import DashboardApp from './pages/DashboardApp';
-import { VerifyEmail } from './pages/VerifyEmail';
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const LandingApp = lazy(() => import("./pages/LandingApp"));
+const DashboardApp = lazy(() => import("./pages/DashboardApp"));
+const VerifyEmail = lazy(() => import("./pages/VerifyEmail"));
+
+export default function App() {
   return (
+    <Suspense fallback={null}>
       <Routes>
-        {/* Landing page */}
+        {/* Landing Page */}
         <Route path="/" element={<LandingApp />} />
 
-        {/* Dashboard system */}
+        {/* Dashboard System */}
         <Route path="/app/*" element={<DashboardApp />} />
 
+        {/* Email Verification */}
         <Route path="/verify-email" element={<VerifyEmail />} />
       </Routes>
+    </Suspense>
   );
 }
-
-export default App;
-
-

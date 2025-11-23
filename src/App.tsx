36bf054268d9d4ad19457dd8473399841769a354
Dimas Darma <dimasdarma35@gmail.com>
2025-11-23 22:50:34 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const LandingApp = lazy(() => import("./pages/LandingApp"));
-const DashboardApp = lazy(() => import("./pages/DashboardApp"));
 const VerifyEmail = lazy(() => import("./pages/VerifyEmail"));
 
 export default function App() {
@@ -11,9 +10,6 @@ export default function App() {
       <Routes>
         {/* Landing Page */}
         <Route path="/" element={<LandingApp />} />
-
-        {/* Dashboard System */}
-        <Route path="/app/*" element={<DashboardApp />} />
 
         {/* Email Verification */}
         <Route path="/verify-email" element={<VerifyEmail />} />

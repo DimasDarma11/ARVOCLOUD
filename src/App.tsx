6f496fb403c2cb2de49e22696012bf3e19b83817
Dimas Darma <dimasdarma35@gmail.com>
2025-12-19 08:40:28 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const LandingApp = lazy(() => import("./pages/LandingApp"));
+const PricingPage = lazy(() => import("./pages/PricingPage"));
 const VerifyEmail = lazy(() => import("./pages/VerifyEmail"));
 
 export default function App() {
   return (
     <Suspense fallback={null}>
       <Routes>
-        {/* Landing Page */}
+        {/* Homepage */}
         <Route path="/" element={<LandingApp />} />
+
+        {/* Pricing Page */}
+        <Route path="/pricing" element={<PricingPage />} />
 
         {/* Email Verification */}
         <Route path="/verify-email" element={<VerifyEmail />} />
